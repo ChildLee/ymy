ce23a33ab8465da24b29b629090c152399c87303
ChildLee <471227142@qq.com>
2017-12-25 22:58:10 +0800

    事务

--- src/main/java/com/ymy/Application.java
+++ src/main/java/com/ymy/Application.java
@@ -3,8 +3,10 @@ package com.ymy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @MapperScan("com.ymy.mapper")
 public class Application {
     public static void main(String[] args) {
--- src/main/java/com/ymy/mapper/AdminMapper.java
+++ src/main/java/com/ymy/mapper/AdminMapper.java
@@ -1,9 +1,11 @@
 package com.ymy.mapper;
 
 import com.ymy.model.Admin;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface AdminMapper {
     int deleteByPrimaryKey(Integer id);
 
