c5398b17d9759b205a4152f0bbd753b1abe52d3c
ChildLee <471227142@qq.com>
2018-01-07 01:46:28 +0800

    添加log4j2

---
+++ src/test/java/com/ymy/LogTests.java
@@ -0,0 +1,23 @@
+package com.ymy;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Log4j2
+public class LogTests {
+
+    @Test
+    public void log() {
+        log.trace("trace level");
+        log.debug("debug level");
+        log.info("info level");
+        log.warn("warn level");
+        log.error("error level");
+        log.fatal("fatal level");
+    }
+}
