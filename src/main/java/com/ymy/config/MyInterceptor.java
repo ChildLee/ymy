6369bc7fb2589aa1227e4eb51d979a75dcaf15d5
ChildLee <471227142@qq.com>
2018-01-10 19:11:28 +0800

    修改拦截器

--- src/main/java/com/ymy/config/MyInterceptor.java
+++ src/main/java/com/ymy/config/MyInterceptor.java
@@ -11,17 +11,17 @@ import javax.servlet.http.HttpServletResponse;
 public class MyInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("1");
+        System.out.println("1.请求处理之前");
         return true;// 只有返回true才会继续向下执行，返回false取消当前请求
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("2");
+        System.out.println("2.请求处理之后，视图被渲染之前调用");
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        System.out.println("3");
+        System.out.println("3.视图被渲染之后");
     }
 }
